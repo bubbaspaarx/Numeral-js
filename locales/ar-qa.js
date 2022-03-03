6a133b2f23aae6d3614019caa2b0987d00f4b34d
Edward Sparks <edd_sparks@hotmail.com>
2022-03-03 16:55:35 +0000

    Ability to use the Qatari Riyal
    Ability to use the Qatari Riyal

---
+++ locales/ar-qa.js
@@ -0,0 +1,32 @@
+// numeral.js locale configuration
+// locale : Qatar (ar-qa)
+// author : Edward Sparks
+
+(function (global, factory) {
+  if (typeof define === 'function' && define.amd) {
+      define(['../numeral'], factory);
+  } else if (typeof module === 'object' && module.exports) {
+      factory(require('../numeral'));
+  } else {
+      factory(global.numeral);
+  }
+}(this, function (numeral) {
+  numeral.register('locale', 'ar-qa', {
+      delimiters: {
+          thousands: '.',
+          decimal: '٫'
+      },
+      abbreviations: {
+          thousand: 'k',
+          million: 'm',
+          billion: 'bn',
+          trillion: 'tn'
+      },
+      ordinal: function (number) {
+          return '';
+      },
+      currency: {
+          symbol: 'ر.ق.'
+      }
+  });
+}));
