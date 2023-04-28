5ac250a6dc7d4624d3b930b325a9ec420fbc3442
Carlos Diaz <72442639+Carlos-Diaz-07@users.noreply.github.com>
2023-04-28 17:29:03 +0100

    Update en-gb.js

--- locales/en-gb.js
+++ locales/en-gb.js
@@ -2,13 +2,13 @@
 // locale : english united kingdom (uk)
 // author : Dan Ristic : https://github.com/dristic
 
-(function (global, factory) {
+(function (globl, factory) {
     if (typeof define === 'function' && define.amd) {
         define(['../numeral'], factory);
     } else if (typeof module === 'object' && module.exports) {
         factory(require('../numeral'));
     } else {
-        factory(global.numeral);
+        factory(globl['numeral']);
     }
 }(this, function (numeral) {
     numeral.register('locale', 'en-gb', {
