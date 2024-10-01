016e3db7a631e9ed05252832826d0ef60d73bd2d
Ed Sparks <edd_sparks@hotmail.com>
2024-10-01 13:18:45 +0100

    added no-nk and se-sk locales

---
+++ locales/no-nk.js
@@ -0,0 +1,32 @@
+// numeral.js locale configuration
+// locale : norwegian krone (dk)
+// author : Edward Sparks
+
+;(function (global, factory) {
+  if (typeof define === 'function' && define.amd) {
+    define(['../numeral'], factory)
+  } else if (typeof module === 'object' && module.exports) {
+    factory(require('../numeral'))
+  } else {
+    factory(global.numeral)
+  }
+})(this, function (numeral) {
+  numeral.register('locale', 'no-nk', {
+    delimiters: {
+      thousands: '.',
+      decimal: ',',
+    },
+    abbreviations: {
+      thousand: 'k',
+      million: 'm',
+      billion: 'b',
+      trillion: 't',
+    },
+    ordinal: function (number) {
+      return '.'
+    },
+    currency: {
+      symbol: 'NOK',
+    },
+  })
+})
